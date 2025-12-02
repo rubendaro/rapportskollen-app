583a2d56e2041823d776f4ba615b17bb7367ee05
Ruben Villegas <rubenvillegas@Rubens-MacBook-Pro.local>
2025-12-02 09:50:43 +0000

    Include ios folder (unignored)

---
+++ ios/RapportskollenClip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RapportskollenClip
+//
+//  Created by Ruben Villegas on 2025-12-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/RapportskollenClip/RapportskollenClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapportskollenClipApp.swift
+//  RapportskollenClip
+//
+//  Created by Ruben Villegas on 2025-12-02.
+//
+
+import SwiftUI
+
+@main
+struct RapportskollenClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
